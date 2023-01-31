3c48c198f967216efd659c066028ecfbc2261943
AravinthThangam <aravinthmano94@gmail.com>
2023-01-31 21:42:36 +0530

    new commit

---
+++ Example.java
@@ -0,0 +1,8 @@
+package java8;
+@FunctionalInterface
+public interface Example {
+	public void printIsRoadways(boolean isRoadways);
+	public static String printType() {
+		return "It is RoadWays Vehicle";
+	}
+}
